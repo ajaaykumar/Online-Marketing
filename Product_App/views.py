306fb8229434d0c6277dbff98ff5894dd3b8e8d7
Ajay <Ajaaykumar777@gmail.com>
2018-12-12 12:50:56 +0530

    in index.html changes in category

--- Product_App/views.py
+++ Product_App/views.py
@@ -22,7 +22,8 @@ def showindex(request):
 def showcategory(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(status='published')
+    # products = Product.objects.filter(status='published')
+    products = Product.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category)
